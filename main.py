34c3a5e92d72db600373a2a79080d8ddc36c47a9
megan88 <59366386+megan88@users.noreply.github.com>
2020-03-25 22:39:54 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,13 +4,11 @@ from scrapy.utils.project import get_project_settings
 
 
 def main():
-    target_urls = [
-        'https://www.ptt.cc/bbs/Tainan/M.1584809867.A.71B.html',
-        'https://www.ptt.cc/bbs/Tainan/M.1584791855.A.D9B.html'
-    ]
+    target_board = ['TAINAN']
     process = CrawlerProcess(get_project_settings())
-    process.crawl('PTTCrawler', start_urls=target_urls, filename='test.json')
-    process.start()
+    for board in target_board:
+        process.crawl('PTTCrawler', board=board)
+        process.start()
 
 if __name__ == '__main__':
     main()
